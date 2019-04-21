a2105272009a7af7c7e357c9b5b90b4a2acd8cfb
于勒 <cdyule@jd.com>
2019-04-22 01:44:58 +0800

    feat: use process.env to get mode

--- trivialities/majorTransformer/index.js
+++ trivialities/majorTransformer/index.js
@@ -3,4 +3,7 @@ const rules = require('./rules');
 const MajorTransformer = require('./MajorTransformer');
 
 const filePath = path.resolve(__dirname, './海滩A调.txt');
-new MajorTransformer(rules, 'A').run(filePath);
+// FIXME: 在mac平台下可使用这种方法获取命令行参数，windows下不知道，有待尝试
+const mode = process.argv[2];
+
+new MajorTransformer(rules, mode).run(filePath);
