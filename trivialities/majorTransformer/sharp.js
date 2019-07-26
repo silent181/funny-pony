1e70d42d25a8a7c6a9a43fc6b57c615f0ae38ab3
于勒 <cdyule@jd.com>
2019-07-26 23:00:49 +0800

    refactor: minor refactor

--- trivialities/majorTransformer/sharp.js
+++ trivialities/majorTransformer/sharp.js
@@ -1,3 +1,8 @@
+/**
+ * 转C大调核心逻辑实现
+ * v0.0.1 增加原调带有升降号的情形 ————2019.07.26
+ */
+
 const { setState } = require('./utils/common');
 const {
     MAX_SHARPED_NOTE,
@@ -18,10 +23,6 @@ const {
     getFlatedPrefixAndSuffix
 } = require('./utils/prefixAndSuffixUtil');
 
-/**
- * 转C大调核心逻辑实现
- * v0.0.1 增加原调带有升降号的情形 ————2019.07.26
- */
 function sharp(noteStr, {
     dist,
     action,
@@ -33,61 +34,51 @@ function sharp(noteStr, {
         note,
         suffix
     ] = getOriginalNoteInfo(noteStr);
-
     const noteNumber = +note;
-
-    return _move();
+    const sharpedNote = noteNumber + dist;
+    let result = {
+        decorator,
+        prefix,
+        suffix,
+        note: sharpedNote
+    };
 
     /**
-     * TODO: 返回转调后的最终结果，包含高低音记号，升降号和音高
-     * 过程略复杂，先用POP编程实现功能
+     * step1：get movedNote, fixedPrefix and fixedSuffix by simple dist move
      */
-    function _move() {
-        const sharpedNote = noteNumber + dist;
-        let result = {
-            decorator,
-            prefix,
-            suffix,
-            note: sharpedNote
-        };
-
-        /**
-         * step1：get movedNote, fixedPrefix and fixedSuffix by simple dist move
-         */
-        if (sharpedNote > MAX_SHARPED_NOTE) {
-            const [sharpedPrefix, sharpedSuffix] = getSharpedPrefixAndSuffix(prefix, suffix);
-            setState(result, {
-                note: sharpedNote - MAX_SHARPED_NOTE,
-                prefix: sharpedPrefix,
-                suffix: sharpedSuffix
-            });
-        }
-
-        /**
-         * step2: check if movedNote should sharp or flat to get final note, decorator, prefix and suffix
-         */
-        if (notesWillChange.includes(getNoteByNoteNumber(result.note))) {
-            if (action === ACTIONS.sharp) {
-                result = sharpHalfKey(result);
-            } else if (action === ACTIONS.flat) {
-                result = flatHalfKey(result);
-            }
-        } else {
-            const args = {
-                prefix: result.prefix,
-                decorator: DECORATORS.none,
-                note: result.note,
-                suffix: result.suffix
-            };
-            if (decorator === DECORATORS.sharp) {
-                result = sharpHalfKey(args);
-            } else if (decorator === DECORATORS.flat) {
-                result = flatHalfKey(args);
-            }
-        }
-
-        return `${result.prefix}${result.decorator}${result.note}${result.suffix}`
+    if (sharpedNote > MAX_SHARPED_NOTE) {
+        const [sharpedPrefix, sharpedSuffix] = getSharpedPrefixAndSuffix(prefix, suffix);
+        setState(result, {
+            note: sharpedNote - MAX_SHARPED_NOTE,
+            prefix: sharpedPrefix,
+            suffix: sharpedSuffix
+        });
     }
+
+    /**
+     * step2: check if movedNote should sharp or flat to get final note, decorator, prefix and suffix
+     */
+    if (notesWillChange.includes(getNoteByNoteNumber(result.note))) {
+        if (action === ACTIONS.sharp) {
+            result = sharpHalfKey(result);
+        } else if (action === ACTIONS.flat) {
+            result = flatHalfKey(result);
+        }
+    } else {
+        const args = {
+            prefix: result.prefix,
+            decorator: DECORATORS.none,
+            note: result.note,
+            suffix: result.suffix
+        };
+        if (decorator === DECORATORS.sharp) {
+            result = sharpHalfKey(args);
+        } else if (decorator === DECORATORS.flat) {
+            result = flatHalfKey(args);
+        }
+    }
+
+    return `${result.prefix}${result.decorator}${result.note}${result.suffix}`
 }
 
 function sharpHalfKey({
