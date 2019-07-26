7e93801f17e087be2ceefbcfc31da4df892337e9
于勒 <cdyule@jd.com>
2019-07-26 23:13:51 +0800

    test: fix test script

--- trivialities/majorTransformer/test/index.js
+++ trivialities/majorTransformer/test/index.js
@@ -3,11 +3,11 @@
  */
 
 const sharp = require('../sharp');
-const rules = require('../consts/rules');
+const { rules } = require('../consts');
 const mode = process.argv[2];
 const rule = rules[mode];
-const testNote = ['#5', '#6', '#7', '#4']
-const expected = ['1.', '2.', '3.', '#6']
+const testNote = ['#5', '#6', '#7', '#4', '6']
+const expected = ['1.', '2.', '3.', '#6', '#1.'] // 以E大调为例，因为E大调升号较多
 
 for (let i = 0; i < testNote.length; i++) {
     const result = sharp(testNote[i], rule);
