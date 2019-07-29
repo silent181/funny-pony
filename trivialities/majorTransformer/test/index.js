41116b85cc184a75f3d1f1dcc2f4832daf0804df
yule57 <cdyule@jd.com>
2019-07-29 14:32:39 +0800

    test: add mocha to test

--- trivialities/majorTransformer/test/index.js
+++ trivialities/majorTransformer/test/index.js
@@ -1,16 +1,33 @@
-/**
- * FIXME: use mocha to test
- */
+const expect = require('chai').expect;
 
 const sharp = require('../sharp');
 const { rules } = require('../consts');
-const mode = process.argv[2];
-const rule = rules[mode];
+
 const testNote = ['#5', '#6', '#7', '#4', '6']
 const expected = ['1.', '2.', '3.', '#6', '#1.'] // 以E大调为例，因为E大调升号较多
 
-for (let i = 0; i < testNote.length; i++) {
-    const result = sharp(testNote[i], rule);
-    console.log('we get: ', result);
-    console.log('expected: ', expected[i]);
-}
+
+const D = rules['D'];
+const E = rules['E'];
+const F = rules['F'];
+const G = rules['G'];
+const A = rules['A'];
+const B = rules['B'];
+
+describe('E大调测试', () => {
+    it(' #5 应该转为 1. ', () => {
+        expect(sharp('#5', E)).to.be.equal('1.');
+    });
+    it(' #6 应该转为 2. ', () => {
+        expect(sharp('#6', E)).to.be.equal('2.');
+    });
+    it(' #7 应该转为 3. ', () => {
+        expect(sharp('#7', E)).to.be.equal('3.');
+    });
+    it(' #4 应该转为 #6 ', () => {
+        expect(sharp('#4', E)).to.be.equal('#6');
+    });
+    it(' 6 应该转为 #1. ', () => {
+        expect(sharp('6', E)).to.be.equal('#1.');
+    });
+});
