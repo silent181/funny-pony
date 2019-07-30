472b92f9201bc572b084310bcd752d77bc90f302
yule57 <cdyule@jd.com>
2019-07-30 16:49:39 +0800

    test: add getOriginalNoteInfo test

--- trivialities/majorTransformer/utils/noteUtil/noteUtil.test.js
+++ trivialities/majorTransformer/utils/noteUtil/noteUtil.test.js
@@ -5,6 +5,14 @@ const {
     getOriginalNoteInfo
 } = require('./noteUtil');
 
+function hasNoNumber() {
+    return getOriginalNoteInfo('..b.');
+}
+
+function invalidInput() {
+    return getOriginalNoteInfo('s1');
+}
+
 describe('getNoteByNoteNumber函数测试', () => {
     it('do', () => {
         expect(getNoteByNoteNumber(1)).to.be.equal('do');
@@ -40,5 +48,47 @@ describe('getNoteByNoteNumber函数测试', () => {
 
     it('unknown', () => {
         expect(getNoteByNoteNumber(123)).to.be.equal(undefined);
+    });
+});
+
+describe('getOriginalNoteInfo函数测试', () => {
+    it('has no number', () => {
+        expect(hasNoNumber).to.throw('note must be a string which contains one number');
+    });
+
+    it('invalid input', () => {
+        expect(invalidInput).to.throw('invalid input');
+    });
+
+    it('have members', () => {
+        expect(getOriginalNoteInfo('.b5')).to.have.members(['.', '', 'b', '5']);
+    });
+
+    it('normal note', () => {
+        expect(getOriginalNoteInfo('5')).to.have.ordered.members(['', '', '5', '']);
+    });
+
+    it('note with sharp', () => {
+        expect(getOriginalNoteInfo('#5')).to.have.ordered.members(['', '#', '5', '']);
+    });
+
+    it('note with flat', () => {
+        expect(getOriginalNoteInfo('b5')).to.have.ordered.members(['', 'b', '5', '']);
+    });
+
+    it('note with prefix', () => {
+        expect(getOriginalNoteInfo('..5')).to.have.ordered.members(['..', '', '5', '']);
+    });
+
+    it('note with suffix', () => {
+        expect(getOriginalNoteInfo('5..')).to.have.ordered.members(['', '', '5', '..']);
+    });
+    
+    it('note with prefix and decorator', () => {
+        expect(getOriginalNoteInfo('.#5')).to.have.ordered.members(['.', '#', '5', '']);
+    });
+
+    it('note with suffix and decorator', () => {
+        expect(getOriginalNoteInfo('b5..')).to.have.ordered.members(['', 'b', '5', '..']);
     });
 });
