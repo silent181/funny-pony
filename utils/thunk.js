64cc99a2200e43a674aba2bda94dce0270d3ff1a
yule57 <cdyule@jd.com>
2019-07-31 16:52:16 +0800

    feat(utils): add thunk.js and co.js

---
+++ utils/thunk.js
@@ -0,0 +1,9 @@
+var thunk = function (fn) {
+    return function () {
+        var args = Array.prototype.slice.call(arguments);
+        return function (callback) {
+            args.push(callback);
+            return fn.apply(this, args);
+        }
+    };
+};
