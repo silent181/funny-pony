14ddba220c87c1fb2e304bd65ef45b2c9e1cc29c
yule57 <cdyule@jd.com>
2019-11-11 10:52:17 +0800

    feat: 压缩字符串

--- leetCode/compress.js
+++ leetCode/compress.js
@@ -17,26 +17,23 @@ var compress = function(chars) {
         if (cur === pre) {
             repeat += 1;
         } else {
-            // compressed.push(repeat.toString().split(''));
-            // compressed.push(cur);
-            compressed = compressed.concat(
-                repeat.toString().split('').concat([cur])
-            )
-            repeat = 1;
+            if (repeat === 1) {
+                compressed.push(cur);
+            } else {
+                compressed = compressed.concat(
+                    repeat.toString().split('').concat([cur])
+                )
+                repeat = 1;
+            }
         }
 
-        if (i === chars.length - 1) {
-            compressed.push(repeat.toString().split(''))
+        if (i === chars.length - 1 && repeat > 1) {
+            compressed = compressed.concat(repeat.toString().split(''))
         }
     }
     // 需要这样赋值，不然提交通不过
     for (var i = 0; i < compressed.length; i++) {
         chars[i] = compressed[i].toString();
     }
-    console.log(compressed);
     return compressed.length;
 };
-
-// var input =["a","a","b","b","c","c","c"];
-var input =["a","b","b","b","b","b","b","b","b","b","b","b","b"];
-compress(input)
