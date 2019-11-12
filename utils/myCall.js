97c9fce93104e4dcf5a975060eeae45ecd539e42
yule57 <cdyule@jd.com>
2019-11-12 22:33:40 +0800

    docs: 为call的实现里面难理解之处加上注释

--- utils/myCall.js
+++ utils/myCall.js
@@ -1,7 +1,14 @@
 Function.prototype.myCall = function (context, ...args) {
     debugger
     context = context || {};
-    context.fn = this;
+    context.fn = this; /**
+     * 这里的this是一个function
+     * 比如functionA.call(context, arg1)这种调用方式，this就是比如functionA
+     * 造成这种比较反直觉的原因在于someFunction.call()这种调用方式与我们平时习惯的someObj.method1()这种调用方式不太一样
+     * 其实只需要深刻理解在函数调用中，this到底等于什么
+     * 例如：someObj.method1()，在method1的作用域中，this === someObj这个比较好理解
+     * 同理：someFunction.call()，在call的作用域中，this === someFunction
+     */
     var res = context.fn(...args);
     delete context.fn;
     return res;
