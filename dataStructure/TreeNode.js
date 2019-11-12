9e5cf9bbfe23211819a2dccf7e81cb460bcd2ab7
yule57 <cdyule@jd.com>
2019-11-12 15:42:08 +0800

    feat: 二叉树中序遍历、先序遍历、后续遍历

--- dataStructure/TreeNode.js
+++ dataStructure/TreeNode.js
@@ -19,3 +19,34 @@ var TreeNode = {
         right: null
     }
 }
+
+/**
+ * 中序遍历（前序遍历）
+ */
+var inOrderTraverse = function (node) {
+    node.left && inOrderTraverse(node.left);
+    console.log(node.val);
+    node.right && inOrderTraverse(node.right);
+}
+
+/**
+ * 先序遍历
+ */
+var preOrderTraverse = function (node) {
+    console.log(node.val);
+    node.left && preOrderTraverse(node.left);
+    node.right && preOrderTraverse(node.right);
+}
+
+/**
+ * 后序遍历
+ */
+var postOrderTraverse = function (node) {
+    node.left && postOrderTraverse(node.left);
+    node.right && postOrderTraverse(node.right);
+    console.log(node.val);
+}
+
+inOrderTraverse(TreeNode) // 4 2 5 1 3
+preOrderTraverse(TreeNode) // 1 2 4 5 3
+postOrderTraverse(TreeNode) // 4 5 2 3 1
