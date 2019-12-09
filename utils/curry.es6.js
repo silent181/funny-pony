186227de091f3d70836cb026c2bb9424b1f10710
yule57 <cdyule@jd.com>
2019-12-09 17:02:19 +0800

    feat: curry——ES6

---
+++ utils/curry.es6.js
@@ -0,0 +1,6 @@
+/**
+ * ES6 一行代码实现curry函数
+ */
+const curry = (fn, args = []) => 
+    (...arg) => 
+        args.concat(arg).length === fn.length ? fn.apply(null, args.concat(arg)) : curry(fn, args.concat(arg));
